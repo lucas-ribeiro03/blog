0863ccef736c0c119d4c3f25a80cc9f53bcb9910
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2025-12-31 13:36:03 -0300

    created at and updated at

--- data/categories.ts
+++ data/categories.ts
@@ -2,6 +2,8 @@ export type Category = {
   nome: string;
   totalLikes: number;
   quantidadePosts: number;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export const categories: Category[] = [
@@ -9,20 +11,28 @@ export const categories: Category[] = [
     nome: "Performance",
     totalLikes: 203,
     quantidadePosts: 12,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
   {
     nome: "Programação",
     totalLikes: 156,
     quantidadePosts: 18,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
   {
     nome: "Tecnologia",
     totalLikes: 124,
     quantidadePosts: 15,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
   {
     nome: "Design",
     totalLikes: 89,
     quantidadePosts: 10,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
 ];
