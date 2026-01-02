4ee2d9614fa14b7abf10061683813464d569182b
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-01-02 16:52:01 -0300

    dropdown components on navbar actions

---
+++ components/profile-dropdown.tsx
@@ -0,0 +1,35 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { User } from "lucide-react";
+
+export const ProfileDropdown = () => {
+  console.log("oia");
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-slate-600 hover:text-blue-600 hover:bg-blue-50 dark:text-slate-400 dark:hover:text-blue-400 dark:hover:bg-blue-950"
+          aria-label="Acessar perfil do usuário"
+          title="Perfil"
+        >
+          <User className="h-5 w-5" aria-hidden="true" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="" align="start">
+        <DropdownMenuLabel>Perfil</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Editar</DropdownMenuItem>
+        <DropdownMenuItem className="text-red-500">Sair</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
---
+++ components/settings-dropdown.tsx
@@ -0,0 +1,103 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Settings } from "lucide-react";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { BR, US } from "country-flag-icons/react/1x1";
+
+function getInitialTheme() {
+  if (typeof window === "undefined") return false;
+  return localStorage.getItem("theme") === "dark";
+}
+
+export const SettingsDropdown = () => {
+  const [showLanguageDialog, setShowLanguageDialog] = useState(false);
+  // TODO   const [languageSelected, setLanguageSelected] = useState<"BR" | "US">("BR");
+  //TODO NOTIFICAÇÕES
+
+  const [isDark, setIsDark] = useState(getInitialTheme);
+
+  const toggleDarkTheme = () => {
+    const newValue = !isDark;
+    setIsDark(newValue);
+    localStorage.setItem("dark", newValue ? "dark" : "light");
+    document.documentElement.classList.toggle("dark", newValue);
+  };
+
+  return (
+    <>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-slate-600 hover:text-blue-600 hover:bg-blue-50 dark:text-slate-400 dark:hover:text-blue-400 dark:hover:bg-blue-950"
+            aria-label="Acessar configurações do usuário"
+            title="Configurações"
+          >
+            <Settings className="h-5 w-5" aria-hidden="true" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 " align="start">
+          <DropdownMenuLabel>Configurações</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuCheckboxItem
+            checked={isDark}
+            onCheckedChange={toggleDarkTheme}
+          >
+            Tema claro
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem>Notificações</DropdownMenuCheckboxItem>
+          <DropdownMenuItem
+            className="ml-6"
+            onSelect={() => setShowLanguageDialog(true)}
+          >
+            Linguagem
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog open={showLanguageDialog} onOpenChange={setShowLanguageDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Selecionar linguagem</DialogTitle>
+            <DialogDescription>
+              Altere para a linguagem de sua preferência
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex justify-around">
+            <div className="bg-slate-700 rounded-md hover:bg-slate-800 transition duration-200 cursor-pointer group">
+              <US className="w-20 h-20 rounded-full cursor-pointer p-1 group-hover:brightness-50 transition" />
+            </div>
+            <div className="bg-slate-700 rounded-md hover:bg-slate-800 transition duration-200 group cursor-pointer">
+              <BR className="w-20 h-20 rounded-full  p-1 group-hover:brightness-50 transition" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button className="hover:text-red-500" variant={"outline"}>
+                Cancelar
+              </Button>
+            </DialogClose>
+            <Button>Confirmar</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
