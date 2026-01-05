d6cb623d074fcc19143928e211192ae1f8931899
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-01-05 12:52:42 -0300

    new tables and columns

--- db/schemas.ts
+++ db/schemas.ts
@@ -1,4 +1,27 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {
+  integer,
+  primaryKey,
+  sqliteTable,
+  text,
+} from "drizzle-orm/sqlite-core";
+
+export const usersTable = sqliteTable("users", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  lastName: text("last_name").notNull(),
+  username: text("username").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+  role: text("role").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
+});
+
+export const categoriesTable = sqliteTable("categories", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull().unique(),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
+});
 
 export const postsTable = sqliteTable("posts", {
   id: text("id").primaryKey(),
@@ -7,17 +30,28 @@ export const postsTable = sqliteTable("posts", {
   excerpt: text("excerpt").notNull(),
   content: text("content").notNull(),
   coverImage: text("cover_image").notNull(),
-  category: text("category").notNull(),
-  createdAt: text("created_at").notNull(),
-  updatedAt: text("updated_at").notNull(),
-  likes: integer("likes").notNull().default(0),
+  categoryId: text("category_id")
+    .notNull()
+    .references(() => categoriesTable.id, { onUpdate: "cascade" }),
+  authorId: text("author_id")
+    .notNull()
+    .references(() => usersTable.id, { onUpdate: "cascade" }),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
 });
 
-export const categoriesTable = sqliteTable("categories", {
-  id: text("id").primaryKey(),
-  name: text("name").notNull(),
-  createdAt: text("created_at").notNull(),
-  updatedAt: text("updated_at").notNull(),
-  quantidadePosts: integer("quantidade_posts").notNull().default(0),
-  totalLikes: integer("total_likes").notNull().default(0),
-});
+export const likesTable = sqliteTable(
+  "likes",
+  {
+    postId: text("post_id")
+      .notNull()
+      .references(() => postsTable.id, { onDelete: "cascade" }),
+    userId: text("user_id")
+      .notNull()
+      .references(() => usersTable.id, { onDelete: "cascade" }),
+    createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.postId, t.userId] }),
+  })
+);
