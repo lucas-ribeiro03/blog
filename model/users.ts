205c5acded6aa1169ed0cd5f8ead85d9aadafcdc
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-01-05 15:09:31 -0300

    users model

---
+++ model/users.ts
@@ -0,0 +1,10 @@
+export type Users = {
+  id: string;
+  name: string;
+  lastName: string;
+  username: string;
+  passwordHash: string;
+  role: string;
+  createdAt: number;
+  updatedAt: number;
+};
