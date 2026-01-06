c654ddaa7809fd441042285d9f79e956b78e647a
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-01-06 16:27:31 -0300

    remove error

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <script
           dangerouslySetInnerHTML={{
@@ -31,10 +31,10 @@ export default function RootLayout({
 (function () {
   try {
     const theme = localStorage.getItem("theme");
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
+    if (theme === "light") {
       document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
     }
   } catch (e) {}
 })();
