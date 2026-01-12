0336884adc038559d2317fa0b0c1518574f70f30
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-01-12 16:42:20 -0300

    logout action

---
+++ actions/AuthActions/logout-action.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function logoutAction() {
+  const cookieStore = await cookies();
+  cookieStore.delete("loginSession");
+  redirect("/");
+}
