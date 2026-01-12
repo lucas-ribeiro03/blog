2d351aaeaa4cbef1e25e410fb4a2734819acb861
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-01-12 16:39:49 -0300

    page is only available for admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,11 @@
 import { AdminMenu } from "@/components/admin/admin-menu";
+import { ErrorMessage } from "@/components/error-message";
+import { verifyLogin } from "@/utils/manage-login";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const isAdmin = await verifyLogin();
+  if (isAdmin.payload.role !== "admin") {
+    return <ErrorMessage message="Página restrita" className="mt-22" />;
+  }
   return <AdminMenu />;
 }
