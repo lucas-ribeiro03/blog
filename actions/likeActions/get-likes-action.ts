1e6392d4de1714d75f99fd696b4c2a8371b09641
lucas-ribeiro03 <lucasribeirollr05@gmail.com>
2026-02-05 11:14:54 -0300

    refactor(likes): remove unused get-likes-action
    - Delete getLikesAction as it's no longer needed
    - Batch like queries handle all use cases via getLikesFromPostAction
    - Simplify like actions by removing redundant per-post queries

--- actions/likeActions/get-likes-action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { likesRepository } from "@/repositories/likes";
-import { postRepository } from "@/repositories/post";
-import { verifyLogin } from "@/utils/manage-login";
-
-export const getLikesAction = async () => {
-  const likesOnPost = await 
-};
